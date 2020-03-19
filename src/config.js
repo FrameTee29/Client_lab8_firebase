141bda10add62dfcc2bc288b3cfb96804bbf6907
FrameTee29 <teeraphat123@hotmail.com>
2020-03-19 10:06:09 +0700

    Config firebase

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCKISSfUgjeQvnx9fygtMQ8s_8_42KeKZ8",
+    authDomain: "todo-e7856.firebaseapp.com",
+    databaseURL: "https://todo-e7856.firebaseio.com",
+    projectId: "todo-e7856",
+    storageBucket: "todo-e7856.appspot.com",
+    messagingSenderId: "254922308987",
+    appId: "1:254922308987:web:e127421b3b97da94e591de",
+    measurementId: "G-T0NY9V8VWF"
+  };
+
+export default firebaseConfig;
